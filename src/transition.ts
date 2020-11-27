2fd09cd4c33ed9e29752f86f8b274bd97cdcba60
unknown <appleskiller@163.com>
2020-11-27 12:36:55 +0800

    build: auto commit before release

--- src/transition.ts
+++ src/transition.ts
@@ -1,19 +1,27 @@
 import { isDefined, isEmpty, requestFrame } from 'neurons-utils';
 import { easingFunctions } from './easing';
 
-export interface ITrasition {
-    duration(value: number): ITrasition;
-    tick(callback: (value: number) => void): ITrasition;
-    from(value: number): ITrasition;
-    to(value: number): ITrasition;
-    easing(easing): ITrasition;
-    complete(value?: number): ITrasition;
+export interface ITrasition<T> {
+    duration(value: number): ITrasition<T>;
+    tick(callback: (value: T) => void): ITrasition<T>;
+    from(value: T): ITrasition<T>;
+    to(value: T): ITrasition<T>;
+    easing(easing): ITrasition<T>;
+    complete(value?: T): ITrasition<T>;
     destroy(): void;
 }
 
 type UnregisterFrameTickHandle = () => void
 interface ITicker {
     onTick(callback): UnregisterFrameTickHandle;
+}
+interface IRunningStates {
+    startTime: number;
+    duration: number;
+    from: any;
+    to: any;
+    easing: any;
+    cancelLoop: () => void;
 }
 
 const defaultTicker = {
@@ -30,37 +38,53 @@ const defaultTicker = {
     },
 }
 
-export class Transition implements ITrasition {
+export class TransitionBase<T> implements ITrasition<T> {
     constructor(ticker?: ITicker){
         this._ticker = ticker || defaultTicker;
     }
-    private _ticker: ITicker;
+    protected _ticker: ITicker;
+    protected _from: T;
+    protected _to: T;
 
-    private _from: number;
-    private _to: number;
-    private _rawDuration: number;
     // 实际动画执行时的总时长，这个值可能因为动画中途改变了from、to、duration和easing而临时发生变化
     private _duration: number;
     private _easing;
-    private _callback: (value: number) => void;
+    private _callback: (value: T) => void;
     
-    private _running: boolean = false;
-    private _startTime: number;
+    private _runningStates: IRunningStates;
     private _cancelLoop;
 
     duration(value: number) {
-        if (this._rawDuration === value) return this;
-        this._rawDuration = value;
+        if (this._duration === value) return this;
+        this._duration = value;
         this._tryRun();
         return this;
     }
-    tick(callback: (value: number) => void) {
+    easing(easing) {
+        if (this._easing === easing) return this;
+        this._easing = easing;
+        this._tryRun();
+        return this;
+    }
+    tick(callback: (value: T) => void) {
         if (this._callback === callback) return this;
         this._callback = callback;
         this._tryRun();
         return this;
     }
-    from(value: number) {
+    complete(value?: T) {
+        if (arguments.length) {
+            this._setComplete(value);
+            this._callback(value);
+        } else if (this._runningStates) {
+            const t = ((new Date()).getTime() - this._runningStates.startTime) / this._runningStates.duration;
+            const current = this._tween(this._runningStates.from, this._runningStates.to, t, this._runningStates.easing);
+            this._setComplete(current);
+            this._callback(current);
+        }
+        return this;
+    }
+    from(value: T) {
         if (this._from === value) return this;
         // 立即结束到value
         const to = this._to;
@@ -70,109 +94,96 @@ export class Transition implements ITrasition {
         this._tryRun();
         return this;
     }
-    to(value: number) {
+    to(value: T) {
         if (this._to === value) return this;
-        const old = this._to;
         this._to = value;
-        if (this._running) {
+        if (this._runningStates) {
             // 未完成的部分切换到新的目标值，但不改变时间总长
-            const t = ((new Date()).getTime() - this._startTime) / this._duration;
-            const current = this._calc(this._from, old, t, this._easing);
+            const t = ((new Date()).getTime() - this._runningStates.startTime) / this._runningStates.duration;
+            const current = this._tween(this._runningStates.from, this._runningStates.to, t, this._runningStates.easing);
             if (t >= 1) {
                 this._setComplete(current);
-                if (current !== value) {
+                // 如果新值与当前值不同，则执行一段新动画以继续缓动到目标值
+                if (!this._equals(current, value)) {
                     this._from = current;
                     this._to = value;
                 }
-                this._callback(current);
             } else {
+                // 在剩余的时间内，缓动到新的目标点
                 this._from = current;
-                this._startTime = (new Date()).getTime();
-                this._duration = this._duration - t;
+                // 更新运行状态
+                this._runningStates.startTime = (new Date()).getTime();
+                this._runningStates.duration = this._runningStates.duration - t;
+                this._runningStates.from = this._from;
+                this._runningStates.to = this._to;
             }
-        }
-        this._tryRun();
-        return this;
-    }
-    easing(easing) {
-        if (this._easing === easing) return this;
-        const old = this._easing;
-        this._easing = easing;
-        if (this._running) {
-            // 未完成的部分使用新的缓动函数进行
-            const t = ((new Date()).getTime() - this._startTime) / this._duration;
-            const value = this._calc(this._from, this._to, t, old);
-            if (t >= 1) {
-                this._setComplete(value);
-                this._callback(value);
-            } else {
-                this._from = value;
-                this._startTime = (new Date()).getTime();
-                this._duration = this._duration - t;
-            }
-        }
-        this._tryRun();
-        return this;
-    }
-    complete(value?: number) {
-        if (arguments.length) {
-            this._setComplete(value);
-            this._callback(value);
-        } else if (this._running) {
-            const t = ((new Date()).getTime() - this._startTime) / this._duration;
-            const current = this._calc(this._from, this._to, t, this._easing);
-            this._setComplete(current);
+            // 回调一次
             this._callback(current);
         }
+        this._tryRun();
         return this;
     }
     destroy() {
         this._cancelLoop && this._cancelLoop();
         this._cancelLoop = null;
-        this._running = false;
-        this._startTime = undefined;
+        this._runningStates && this._runningStates.cancelLoop();
+        this._runningStates = null;
     }
-    private _assert() {
+    protected _equals(newValue: T, oldValue: T): boolean {
+        return newValue === oldValue;
+    }
+    protected _assert() {
         return this._from !== undefined
             && this._to !== undefined
-            && this._from !== this._to
+            && !this._equals(this._from , this._to)
             && this._easing !== undefined
             && this._callback !== undefined
-            && this._rawDuration !== undefined;
+            && this._duration !== undefined;
     }
-    private _tryRun() {
-        if (this._running || !this._assert()) return;
-        this._running = true;
-        if (this._startTime === undefined) {
-            this._startTime = (new Date()).getTime();
+    protected _tryRun() {
+        if (this._runningStates || !this._assert()) return;
+        this._runningStates = {
+            duration: this._duration,
+            easing: this._easing,
+            from: this._from,
+            to: this._to,
+            startTime: (new Date()).getTime(),
+            cancelLoop: this._ticker.onTick(this._animate),
         }
-        if (this._duration === undefined) {
-            this._duration = this._rawDuration;
-        }
-        this._cancelLoop && this._cancelLoop();
-        this._cancelLoop = this._ticker.onTick(this._animate);
     }
-    private _onTick() {
-        if (!this._running) return;
-        let t = ((new Date()).getTime() - this._startTime) / this._duration;
-        const value = this._calc(this._from, this._to, t, this._easing);
+    protected _onTick() {
+        if (!this._runningStates) return;
+        let t = ((new Date()).getTime() - this._runningStates.startTime) / this._runningStates.duration;
+        const value = this._tween(this._runningStates.from, this._runningStates.to, t, this._runningStates.easing);
         if (t >= 1) {
             this._setComplete(value);
         }
         this._callback(value);
     }
-    private _setComplete(value) {
-        this._cancelLoop && this._cancelLoop();
-        this._cancelLoop = null;
-        this._running = false;
-        this._startTime = undefined;
-        this._duration = undefined;
+    protected _setComplete(value: T) {
+        this._runningStates && this._runningStates.cancelLoop();
+        this._runningStates = null;
         // 处理from to，便于执行值变更行为所产生的连续动画
         this._from = value;
         this._to = undefined;
     }
     private _animate = this._onTick.bind(this);
-    private _calc(from, to, t, easing): number {
+    protected _tween(from: T, to: T, t, easing): T {
+        if (t >= 1) {
+            // 完成
+            return to;
+        } else {
+            // 子类实现计算缓动结果
+            return from;
+        }
+    }
+}
+
+export class Transition extends TransitionBase<number> {
+    constructor(ticker?: ITicker){
+        super(ticker);
+    }
+    protected _tween(from: number, to: number, t, easing): number {
         if (t >= 1) {
             // 完成
             return to;
@@ -187,59 +198,32 @@ export class Transition implements ITrasition {
     }
 }
 
-export interface IAttributesTransitionOption {
-    object: any;
-    duration?: number;
-    easing?: (t) => number;
-    ticker?: ITicker;
-}
+type Attributes = {[key: string]: number}
 
-export interface IAttributesTransition {
-    set(attrs: any): IAttributesTransition;
-    complete(): IAttributesTransition;
-    destroy(): void;
-}
-
-export class AttributesTransition implements IAttributesTransition {
-    constructor(option: IAttributesTransitionOption) {
-        this._option = {...(option || {})} as IAttributesTransitionOption;
-        this._option.duration = isDefined(this._option.duration) ? this._option.duration : 280;
-        this._option.easing = this._option.easing || easingFunctions.easeOutQuart;
-        this._option.ticker = this._option.ticker || defaultTicker;
+export class AttributesTransition extends TransitionBase<Attributes> {
+    protected _equals(newValue: Attributes, oldValue: Attributes): boolean {
+        if (newValue === oldValue) return true;
+        const newKeys = Object.keys(newValue);
+        return newKeys.every(key => (newValue[key] === oldValue[key]))
     }
-    static create(option: IAttributesTransitionOption): IAttributesTransition {
-        return new AttributesTransition(option);
-    }
-    private _option: IAttributesTransitionOption;
-    private _trasitions: {[key: string]: ITrasition} = {};
-    set(attrs: any): IAttributesTransition {
-        if (!attrs || isEmpty(attrs)) return this;
-        if (!this._option.object) return this;
-        Object.keys(attrs).forEach(key => {
-            if (isDefined(attrs[key])) {
-                if (!this._trasitions[key]) {
-                    this._trasitions[key] = new Transition(this._option.ticker);
-                    this._trasitions[key].from(attrs[key]).easing(this._option.easing).duration(this._option.duration).tick((value) => {
-                        this._set(key, value);
-                    });
+    protected _tween(from: Attributes, to: Attributes, t, easing): Attributes {
+        if (t >= 1) {
+            // 完成
+            return {...to};
+        } else {
+            const v = easing(t);
+            const ret: Attributes = {};
+            Object.keys(to).forEach(key => {
+                const f = from[key], t = to[key];
+                if (f === t) return;
+                if (f > t) {
+                    ret[key] = f - v * (f - t);
+                } else {
+                    ret[key] = f + v * (t - f);
                 }
-                this._trasitions[key].to(attrs[key]);
-            }
-        })
-        return this;
-    }
-    complete(): IAttributesTransition {
-        if (!this._option.object) return this;
-        Object.keys(this._trasitions).forEach(key => this._trasitions[key].complete());
-        return this;
-    }
-    destroy() {
-        Object.keys(this._trasitions).forEach(key => this._trasitions[key].destroy());
-        this._trasitions = {};
-        this._option.object = null;
-    }
-    protected _set(property: string, value: number) {
-        this._option.object[property] = value;
+            })
+            return ret;
+        }
     }
 }
 
